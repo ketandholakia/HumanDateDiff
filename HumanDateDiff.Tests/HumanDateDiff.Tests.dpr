bb8a5a13d0d8b16d14c40284abcf36900ef69239
Ketan Dholakia <ketanmci@gmail.com>
2026-01-10 17:39:58 +0530

    HumanDateDiff: all tests passing, locale-aware humanizer complete

--- HumanDateDiff.Tests/HumanDateDiff.Tests.dpr
+++ HumanDateDiff.Tests/HumanDateDiff.Tests.dpr
@@ -1,13 +1,40 @@
-program HumanDateDiff.Tests;
+﻿program HumanDateDiff.Tests;
 
 {$APPTYPE CONSOLE}
 
 uses
+  System.SysUtils,
+  DUnitX.TestFramework,
   DUnitX.Loggers.Console,
   DUnitX.Loggers.XML.NUnit,
-  DUnitX.TestFramework;
 
+  // 👇 THIS IS REQUIRED
+  TestHumanDateDiff.Core,
+  TestHumanDateDiff.Rules,
+  TestHumanDateDiff.Locales;
+
+var
+  Runner: ITestRunner;
+  Results: IRunResults;
 begin
-  TDUnitX.CheckCommandLine;
-  TDUnitX.Run;
+  try
+    TDUnitX.CheckCommandLine;
+
+    Runner := TDUnitX.CreateRunner;
+    Runner.UseRTTI := True;
+
+    Runner.AddLogger(TDUnitXConsoleLogger.Create(True));
+    Runner.AddLogger(TDUnitXXMLNUnitFileLogger.Create);
+
+    Results := Runner.Execute;
+
+    if not Results.AllPassed then
+      ExitCode := 1;
+  except
+    on E: Exception do
+    begin
+      Writeln(E.ClassName, ': ', E.Message);
+      ExitCode := 1;
+    end;
+  end;
 end.
